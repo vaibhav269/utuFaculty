1f0c2417f2430cf6ca9f2a12047ea4e9d4dedb1f
vaibhav269 <32865508+vaibhav269@users.noreply.github.com>
2017-10-19 18:04:11 +0530

    Add files via upload

--- public/scripts/scriptAdminControls.js
+++ public/scripts/scriptAdminControls.js
@@ -19,3 +19,67 @@ function removeDean(removeBtn){
                                 }
                         });
                 }
+
+function initMap() {
+               var input = document.getElementById('collegeLocation');
+               var searchBox = new google.maps.places.SearchBox(input);
+
+               // Listen for the event fired when the user selects a prediction and retrieve
+               // more details for that place and show the map
+               searchBox.addListener('places_changed', function() {
+                       var places = searchBox.getPlaces();
+
+                       if (places.length == 0) {
+                               return;
+                       }
+
+                       // For each place, get the icon, name and location.
+                       var bounds = new google.maps.LatLngBounds();
+
+                       places.forEach(function(place) {
+                               $("#lat").val(place.geometry.location.lat());    //setting the value of lat on choosing any position
+                               $("#lng").val(place.geometry.location.lng());    //setting the value of lng on choosing any position
+
+                               if (!place.geometry) {
+                                       console.log("Returned place contains no geometry");
+                                       return;
+                               }
+
+                               if (place.geometry.viewport) {
+                                       // Only geocodes have viewport.
+                                       bounds.union(place.geometry.viewport);
+                               } else {
+                                       bounds.extend(place.geometry.location);
+                               }
+                       });
+               });
+     }
+
+$(document).ready(function(){
+                $("#showDeans").on('click',function(){
+                                $(".containers").hide();
+                                $("#deanContainer").show();
+                });
+
+                $("#enterCollegeDetails").on('click',function(){
+                                $(".containers").hide();
+                                $("#collegeDetailsContainer").show();
+                });
+
+                $("#collegeEnterForm").submit(function(event){
+                        event.preventDefault();                                 //to stop form from submitting
+                        var formData=$("#collegeEnterForm").serialize();
+                        $.ajax({
+                                url:"/collegeEnter",
+                                data:formData,     //retriving faculty's _id from confirmBtn's id attribute
+                                type:"post",
+                                success:function(data){
+                                        alert("Successfully entered");
+                                        $("#collegeLocation").val("");
+                                        $("#collegeName").val("");
+                                        $("#lat").val("");
+                                        $("#lng").val("");
+                                        }
+                                });
+                });
+});
